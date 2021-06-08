1056caf39562c70d68631d11202d0d8e7d25e3d6
Sharda <bestprakash.1@gmail.com>
2021-06-08 15:39:29 +0530

    First is a simple program with @test annotation and executed using TestNg

---
+++ testing/src/test/java/test/first.java
@@ -0,0 +1,12 @@
+//Simple testcase trigger from @test and testng
+package test;
+
+import org.testng.annotations.Test;
+
+public class first {
+
+	@Test
+	public void demo() {
+		System.out.println("Hello");
+	}
+}
