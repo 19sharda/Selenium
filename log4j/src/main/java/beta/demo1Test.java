7bf05eee17b4758381f71dcead6bfd1f55196d4e
19sharda <40214015+19sharda@users.noreply.github.com>
2021-06-13 15:48:29 +0530

    Log4j Demonstration project

---
+++ log4j/src/main/java/beta/demo1Test.java
@@ -0,0 +1,24 @@
+package beta;
+import org.apache.logging.log4j.*;
+public class demo1Test {
+
+	private static Logger log=LogManager.getLogger(demo1Test.class.getName());
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=-1;
+		int b=2;
+		
+		log.debug("Dubugging......");
+		if(a<0) {
+			log.info("Object present");
+		}
+		if(b>0) {
+			log.fatal("This is fetal");
+		}
+		
+		if(b>a) {
+			log.error("This is error");
+		}
+	}
+
+}
